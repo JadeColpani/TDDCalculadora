4d9f83c102bcab724e91eea0f5e4e133a6bd5782
Jade Colpani <jade.colpani@fatec.sp.gov.br>
2023-05-10 16:33:26 -0300

    Initial commit

---
+++ src/Calculadora.java
@@ -0,0 +1,19 @@
+public class Calculadora {
+
+    public int soma() {
+        throw new UnsupportedOperationException("Não é possível calcular.");
+    }
+
+    public int subtracao() {
+        throw new UnsupportedOperationException("Não é possível calcular.");
+    }
+
+    public int multiplicacao() {
+        throw new UnsupportedOperationException("Não é possível calcular.");
+    }
+
+    public int divisao() {
+        throw new UnsupportedOperationException("Não é possível calcular.");
+    }
+
+}
